7818391dd31d9847fe4b2645a7eb9bd4729d740a
chrnnsa <chairun089@gmail.com>
2021-11-14 09:40:50 +0700

    pertemuan 3

---
+++ lib/icon_content.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  const IconContent({
+    Key? key,
+    required this.icon,
+    required this.label,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 20.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
